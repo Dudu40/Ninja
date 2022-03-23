84938a04d8144a877f96940bd5bf5367d7269ff4
Dudu4064 <ducasseque@eisti.eu>
2022-03-23 23:15:44 +0100

    dd

--- Assets/Scripts/Player/PlayerStates/Sub States/InAirState.cs
+++ Assets/Scripts/Player/PlayerStates/Sub States/InAirState.cs
@@ -97,9 +97,6 @@ public class InAirState : PlayerState
     }
 
     private void CheckDoubleJump(){
-     if(player.CheckIfTouchingWall() && isJumping){
-       player.JumpState.ResetAmountOfJumpsLeft();
-     }
     }
 
     private void CheckJumpMultiplier()
